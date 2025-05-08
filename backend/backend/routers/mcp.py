ba8162286f160277dbc8b002e6aae55c7a56086a
Shinya Maeda <shinya@gitlab.com>
2025-05-08 09:24:30 +0900

    return dummy mcp result response

--- backend/backend/routers/mcp.py
+++ backend/backend/routers/mcp.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from typing import Annotated, Optional, List, Dict, Any
 from backend.auth.utils import User, get_current_user
 from fastapi import Depends, FastAPI, HTTPException, status, Request, Form
+from backend.mcp.schema import ListToolsResult, Tool
 
 
 def validate_mcp_headers(request: Request):
@@ -17,11 +18,41 @@ router = APIRouter(dependencies=[Depends(validate_mcp_headers)])
 async def mcp_get(
     current_user: Annotated[User, Depends(get_current_user)],
 ):
-    return [{"item_id": "Foo", "owner": current_user.username}]
+    return [{"message": "not implemented"}]
 
 
 @router.post("/mcp")
 async def mcp_post(
     current_user: Annotated[User, Depends(get_current_user)],
 ):
-    return [{"item_id": "Foo", "owner": current_user.username}]
+    result = ListToolsResult(
+        tools=[
+            Tool(
+                name="read_user_blog_post",
+                description="Read a blog post that the user wrote",
+                inputSchema={"blog_post_id": {"type": "str"}},
+            ),
+            Tool(
+                name="create_user_blog_post",
+                description="Create a new blog post",
+                inputSchema={
+                    "content": {
+                        "type": "str",
+                        "description": "Content of the blog post",
+                    }
+                },
+            ),
+            Tool(
+                name="update_user_blog_post",
+                description="Update an existing blog post",
+                inputSchema={
+                    "blog_post_id": {"type": "str"},
+                    "new_content": {
+                        "type": "str",
+                        "description": "New content of the blog post",
+                    },
+                },
+            ),
+        ]
+    )
+    return result.model_dump()
